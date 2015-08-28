9079dc2da95214729d72d47e9964e8dd34f3d2af
cch5ng <cch5ng@gmail.com>
2015-08-27 23:33:22 -0700

    trying to tshoot issue I think with use of this and setInterval. I want this to refer to the timer instance but I think this is refering to the window obj instead

--- js/main.js
+++ js/main.js
@@ -22,8 +22,8 @@ function convertTime(millisecond) {
   min = Math.floor(millisecond / 60000);
   sec = (millisecond % 60000) / 1000;
   
-  console.log('min: ' + min);
-  console.log('sec: ' + sec);
+  //console.log('min: ' + min);
+  //console.log('sec: ' + sec);
   return [min, sec];
 }
 
@@ -31,10 +31,19 @@ var Timer = function(name, minutes, isActive, classStr) {
   this.nameStr = name;
   this.minutes = minutes || 0;
   this.milliseconds = this.minutes * 60000;
+  console.log('this.milliseconds: ' + this.milliseconds)
+  //isActive indicates if this is the current timer displayed in the clock section
   this.isActive = isActive || false;
+  //isCountingDown indicates if the countdown timer is active for this timer
+  this.isCountingDown = false;
   this.classStr = classStr || '';
   this.remainingTime = convertTime(this.milliseconds);
+  this.intervalId = null;
 };
+
+Timer.prototype.getMilliseconds = function() {
+  return this.milliseconds;
+}
 
 //increments timer's minutes property by one and updates minutes display
 Timer.prototype.incMinutes = function() {
@@ -49,16 +58,20 @@ Timer.prototype.decMinutes = function() {
 }
 
 Timer.prototype.countDown = function() {
+  console.log('this: ' + this);
+  console.log(this.milliseconds);
   if (this.milliseconds - 1000 > 0) {
+    this.isActive = true;
+    this.isCountingDown = true;
     this.milliseconds -= 1000;
     this.minutes -= 1;
     //console.log('sessionLength: ' + sessionLength);
     this.remainingTime = convertTime(this.milliseconds);
     this.displayMinutes();
     this.displayRemainingTime();
-    this.isActive = true;
   } else {
-    this.isActive = false;
+    this.isCountingDown = false;
+    //this.isActive = false;
     console.log('play alarm and change to other timer');
   }
 }
@@ -69,7 +82,8 @@ Timer.prototype.start = function() {
 
 Timer.prototype.stop = function() {
   clearInterval(this.intervalId);
-  this.isActive = false;
+  //this.isActive = false;
+  this.isCountingDown = false;
 }
 
 Timer.prototype.reset = function() {
@@ -79,7 +93,7 @@ Timer.prototype.reset = function() {
 
 Timer.prototype.displayMinutes = function() {
   var span = document.querySelector(this.classStr);
-  console.log(span);
+  //console.log(span);
   span.innerHTML = '';
   span.innerHTML = this.minutes;
 }
@@ -97,7 +111,7 @@ Timer.prototype.displayRemainingTime = function() {
   if (sec < 10) {
     sec = '0' + sec;
   }
-  var div = document.querySelector('.remaining-time-inner');
+  var div = document.querySelector('.remaining-time');
   div.innerHTML = '';
   div.innerHTML = min + ':' + sec;
 }
@@ -117,10 +131,31 @@ if (sessionTimer.isActive) {
   breakTimer.displayRemainingTime();
 }
 
+//click handlers
+var toggleTimer = function() {
+  if (sessionTimer.isActive && sessionTimer.isCountingDown) {
+    console.log('sessionTimer.stop');
+    sessionTimer.stop();
+  } else if (sessionTimer.isActive && !sessionTimer.isCountingDown) {
+    console.log('sessionTimer.start');
+    sessionTimer.start();
+  } else if (breakTimer.isActive && breakTimer.isCountingDown) {
+    breakTimer.stop();
+  } else if (breakTimer.isActive && !breakTimer.isCountingDown) {
+    breakTimer.start();
+  }
+};
+
+
 //onclick listeners
-var startBtn = document.getElementById('start');
-startBtn.addEventListener('click', start);
-var stopBtn = document.getElementById('stop');
-stopBtn.addEventListener('click', stop);
-var resetBtn = document.getElementById('reset');
-resetBtn.addEventListener('click', reset);
+var divClock = document.querySelector('.clock');
+divClock.addEventListener('click', toggleTimer);
+
+// var startBtn = document.getElementById('start');
+// startBtn.addEventListener('click', start);
+// var stopBtn = document.getElementById('stop');
+// stopBtn.addEventListener('click', stop);
+
+//TODO, don't have a ui element for this event
+// var resetBtn = document.getElementById('reset');
+// resetBtn.addEventListener('click', reset);
